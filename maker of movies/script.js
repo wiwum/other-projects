7b98105524b7f12af8b4ff282b09beeb12308e09
wiwum <wgward@gmail.com>
2017-04-20 13:55:17 -0500

    Update script.js

--- maker of movies/script.js
+++ maker of movies/script.js
@@ -1,1 +1,5 @@
-
+//variables:
+var im1src = document.getElementById("im1src");
+var im2src = document.getElementById("im2src");
+var im3src = document.getElementById("im3src");
+var im4src = document.getElementById("im4src");
